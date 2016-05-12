2ac60dd4ee293a8b27001c118a31b48777b40dd5
dreamrecord4ever <dreamrecord4ever@gmail.com>
2016-05-13 01:27:23 +0800

    converge feature added: L1 norm

--- pageRank/pageRank.py
+++ pageRank/pageRank.py
@@ -2,13 +2,16 @@
 
 import numpy as np
 
+CONVERGE_THRES = 10**-3
+
 class pageRank:
     
-    def __init__(self, adjMatrix, dampingFactor):
+    def __init__(self, adjMatrix, dampingFactor, convergeThres = CONVERGE_THRES):
         
         # adj matrix
-        self.A = adjMatrix[:]
+        self.A = np.copy(adjMatrix)
         self.dim = self.A.shape[0]
+        self.convergeThres = convergeThres
 
         for row in self.A:
             row /= row.sum()
@@ -23,6 +26,21 @@ class pageRank:
         for i in xrange(k):
             self.P = np.dot(self.A.transpose(), self.P)
 
+    def run(self):
+        
+        numOfIters = 0
+        diff = self.P.sum()
+
+        while diff > self.convergeThres:
+
+            newP = np.dot(self.A.transpose(), self.P)
+            diff = np.linalg.norm(newP-self.P, 1)
+            self.P = np.copy(newP)
+            
+            numOfIters += 1
+
+        return numOfIters 
+
     def printPageRank(self):
         
         print self.P
@@ -30,3 +48,26 @@ class pageRank:
     def printAdjMatrix(self):
         
         print self.A
+
+if __name__ == '__main__':
+    
+    # parameter
+    adjMatrix = np.array([[0, 1, 1, 0, 0],
+                          [0, 0, 0, 1, 1],
+                          [0, 1, 0, 0, 0],
+                          [0, 0, 1, 0, 0],
+                          [0, 0, 0, 1, 0]], dtype=float)
+    dampingFactor = 0.8
+
+    pageRankAlgo = pageRank(adjMatrix, dampingFactor)
+
+    print 'Adjacency Matrix'
+    pageRankAlgo.printAdjMatrix()
+
+    print 'Original Page Rank Value:'
+    pageRankAlgo.printPageRank()
+
+    numOfIters = pageRankAlgo.run()
+
+    print 'Page Rank Value after', numOfIters, 'iters:'
+    pageRankAlgo.printPageRank()
